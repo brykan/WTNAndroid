8891f55b25a7db67cb08d96b9372aba371301966
John Le <johnminhle01@gmail.com>
2018-02-25 20:41:09 -0500

    Renamed files to default and added background

--- app/src/main/java/com/example/bryan/whatsteddysname/MainActivity.java
+++ app/src/main/java/com/example/bryan/whatsteddysname/MainActivity.java
@@ -3,11 +3,11 @@ package com.example.bryan.whatsteddysname;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 }
