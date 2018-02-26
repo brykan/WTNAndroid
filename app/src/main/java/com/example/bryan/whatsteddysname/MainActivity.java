50d90eebe54ab73a27cf33e7c9fbfd05473febcb
John Le <johnminhle01@gmail.com>
2018-02-25 23:42:44 -0500

    Added the start of Login and Signup Activities

--- app/src/main/java/com/example/bryan/whatsteddysname/MainActivity.java
+++ app/src/main/java/com/example/bryan/whatsteddysname/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.bryan.whatsteddysname;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button logInBtn;
+    private Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logInBtn = (Button) findViewById(R.id.logInBtn);
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity("Login");
+            }
+        });
+
+        signUpBtn = (Button) findViewById(R.id.signUpBtn);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity("Sign-up");
+            }
+        });
+    }
+
+    public void openActivity(String activityName) {
+        if(activityName == "Login") {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        } else if (activityName == "Sign-up") {
+            Intent intent = new Intent(this, SignupActivity.class);
+            startActivity(intent);
+        }
     }
 }
