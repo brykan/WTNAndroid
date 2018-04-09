c196b7377b2c945088dcff215f7b891849396f96
John Le <johnminhle01@gmail.com>
2018-04-08 23:53:50 -0400

    Images in collection now get downloaded from s3 if they don't exist locally

--- app/src/main/java/com/example/bryan/whatsteddysname/ItemList.java
+++ app/src/main/java/com/example/bryan/whatsteddysname/ItemList.java
@@ -14,6 +14,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.services.s3.AmazonS3Client;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -35,23 +44,90 @@ public class ItemList extends ArrayAdapter<String> {
         LayoutInflater inflater = context.getLayoutInflater();
         View rowView = inflater.inflate(R.layout.single_item, null, true);
         TextView itemName = (TextView) rowView.findViewById(R.id.item_name);
-        ImageView itemImg = (ImageView) rowView.findViewById(R.id.item_img);
-
+        final ImageView itemImg = (ImageView) rowView.findViewById(R.id.item_img);
+        
         try {
-            JSONObject item = new JSONObject(items.get(position));
+            final JSONObject item = new JSONObject(items.get(position));
             String name = item.getString("itemName");
 
             itemName.setText(name);
 
            File imgFile = new File(item.getString("localPhotoPath"));
 
-           Bitmap bitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            if(imgFile.exists()) {
 
-           itemImg.setImageBitmap(bitmap);
+                Bitmap bitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+                itemImg.setImageBitmap(bitmap);
+            } else {
+                dowloadData(item, itemImg);
+            }
         } catch(JSONException e) {
             Log.d("JSONEXCEPTION", e.getMessage());
         }
 
         return rowView;
     }
+
+    public void dowloadData(final JSONObject item, final ImageView itemImg) {
+        AWSMobileClient.getInstance().initialize(getContext(), new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                downloadWithTransferUtility(item, itemImg);
+            }
+        }).execute();
+    }
+
+    public void downloadWithTransferUtility(final JSONObject item, final ImageView itemImg) {
+
+        TransferUtility transferUtility =
+                TransferUtility.builder()
+                        .context(this.context)
+                        .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
+                        .s3Client(new AmazonS3Client(AWSMobileClient.getInstance().getCredentialsProvider()))
+                        .build();
+        try {
+            TransferObserver downloadObserver =
+                    transferUtility.download(
+                            item.getString("s3Location"),
+                            new File(item.getString("localPhotoPath")));
+
+            // Attach a listener to the observer to get state update and progress notifications
+            downloadObserver.setTransferListener(new TransferListener() {
+                @Override
+                public void onStateChanged(int id, TransferState state) {
+                    if (TransferState.COMPLETED == state) {
+                        // Handle a completed upload.
+                        try {
+                            File imgFile = new File(item.getString("localPhotoPath"));
+
+                            Bitmap bitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+                            itemImg.setImageBitmap(bitmap);
+
+                        } catch(JSONException e) {
+                            Log.d("JSONEXCEPTION", e.getMessage());
+                        }
+                    }
+                }
+
+                @Override
+                public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+                    float percentDonef = ((float) bytesCurrent / (float) bytesTotal) * 100;
+                    int percentDone = (int) percentDonef;
+
+                    Log.d("ItemList", "   ID:" + id + "   bytesCurrent: " + bytesCurrent + "   bytesTotal: " + bytesTotal + " " + percentDone + "%");
+                }
+
+                @Override
+                public void onError(int id, Exception ex) {
+                    Log.d("ItemList #" + id, ex.getMessage());
+                }
+
+            });
+        } catch (JSONException e) {
+            Log.d("JSONEXCEPTION", e.getMessage());
+        }
+    }
+
 }
